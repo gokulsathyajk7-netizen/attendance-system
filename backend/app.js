bf0a0aeb508d0571dc09e1a90c1a15fea878bc8c
Gokul <gokulsathyajk7@gmail.com>
2026-06-23 13:38:33 +0530

    Added home route

--- backend/app.js
+++ backend/app.js
@@ -83,6 +83,14 @@ app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Attendance System API Running Successfully"
+  });
+});
+
 /* =========================
 Logging
 ========================= */
